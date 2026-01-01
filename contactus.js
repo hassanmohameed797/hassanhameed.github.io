50ef1accaee5d44e062d55fe14e9a9ebd7950b9c
Hassan <hassanhameed153@gmail.com>
2026-01-01 03:57:28 +0200

    Update contact methods in contactus.js

--- contactus.js
+++ contactus.js
@@ -1,10 +1,11 @@
 function openFacebook(){
-    window.open("");
+    window.open("https://www.facebook.com/hassan.mohammed.839908","_blank");
 }
 function openW(){
-    window.open("");
+    window.open("https://wa.me/+201001405010","_blank");
 }
 function makeCall(){
-    window.open("");
+    window.open("tel:+201001405010");
 
 }
+
